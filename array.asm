3355783bc28607a9b8ae8cf9d6b12f91ec14b956
maksym <vasyl.lanko@tecnico.ulisboa.pt>
2024-12-26 22:39:20 +0100

    refresher and tweaked array example

--- array.asm
+++ array.asm
@@ -7,6 +7,11 @@ section .text
 	global start
 
 start:
+    call calculate_sum
+    add al, 16              ; add 0x10 to the result
+    jmp end                 
+
+calculate_sum:
     mov  ax, 0              ; Initialize accumulator
     mov  cx, 0              ; Initialize counter
     mov  dx, 0 			  	; Initialize read value
@@ -15,10 +20,13 @@ sum:
     mov  bx, cx             ; Copy counter to bx
     mov  dl, [array + bx]   ; Load array element
     cmp  dl, 0            	; Check for end of array
-    je   end              	; If zero, we're done
+    je   finish              	; If zero, we're done
     add  ax, dx            	; Add to sum
     inc  cx                	; Increment counter
     jmp  sum               	; Continue loop
+
+finish:
+    ret                     ; return
 
 end:
     ; Result is in AX
